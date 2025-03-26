d3e6b07fd47fda341d4ac6fc6f1375dc823e992e
CoderTahmid <tahmid.ibne@gmail.com>
2025-03-26 12:03:11 +0600

    some basic concepts about C is added

---
+++ Basic concepts of C.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/*
+About this "include" keyword
+The #include keyword in C is a preprocessor directive
+that tells the compiler to include the contents of a specified file (usually a header file)
+before compiling the program.
+*/
+
+/*
+Why is main function of type int?
+The main function returns an integer (int). 
+This return value is sent to the operating system to indicate whether the program ran successfully.
+
+Returning 0 means the program completed successfully.
+Returning a non-zero value indicates an error
+*/
+
+int main()
+{
+
+    /*
+    Why we alwasys write this "main" function?
+    In C, execution always starts from the main function.
+    The operating system looks for main as the entry point of my program.
+    */
+
+    printf("King");
+
+    return 0;
+}
--- practicePage.c
+++ practicePage.c
@@ -1,10 +1,4 @@
 #include <stdio.h>
-/*
-About this "include" keyword
-The #include keyword in C is a preprocessor directive 
-that tells the compiler to include the contents of a specified file (usually a header file) 
-before compiling the program.
-*/
 
 int main () {
     printf("King");
