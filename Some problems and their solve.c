aa015b93723d9a1ea53886450f2fe8b483ce2c77
CoderTahmid <tahmid.ibne@gmail.com>
2025-03-27 06:59:04 +0600

    problem 1 and it's solution added in the problem & solution list

--- Some problems and their solve.c
+++ Some problems and their solve.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
-int main()
-{   
+// Problem 1--> Find the Factorial of a Number
+int findFactorial()
+{
     int num;
 
     printf("Enter a number : ");
     scanf("%d", &num);
-    
+
     int factorialNumber = 1;
 
-    for (int i = 1 ; i <= num ; i++) {
+    for (int i = 1; i <= num; i++)
+    {
         factorialNumber = factorialNumber * i;
     }
 
