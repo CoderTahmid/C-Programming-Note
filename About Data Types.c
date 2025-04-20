d50e76f28711794f50cf53b573a1f2145dd16b87
CoderTahmid <tahmid.ibne@gmail.com>
2025-04-20 23:11:13 +0600

    Usage of getchar added

--- About Data Types.c
+++ About Data Types.c
@@ -92,3 +92,23 @@ int using_char_for_characters()
         */
     return 0;
 }
+
+int usage_of_getchar() 
+{
+    char ch;
+   
+    printf("Enter your name:");
+    ch = getchar();
+    /*
+    so basically this getchar() function does the same as the scanf("")
+    function did
+    what we did in scanf() ?
+    we take a input from the user and set the value (by using &)in the 
+    ch variable 
+    and now this getchar is doing the same
+    */
+    
+    printf("The first letter of your name is %c", ch);
+    
+    return 0;
+}
--- practicePage.c
+++ practicePage.c
@@ -1,6 +1,21 @@
-#include<stdio.h> 🔴
+#include<stdio.h>
 
 int main()
 {
-
+   char ch;
+   
+   printf("Enter your name:");
+   ch = getchar();
+   /*
+   so basically this getchar() function does the same as the scanf("")
+   function did
+   what we did in scanf() ?
+   we take a input from the user and set the value (by using &)in the 
+   ch variable 
+   and now this getchar is doing the same
+   */
+   
+   printf("The first letter of your name is %c", ch);
+   
+   return 0;
 }
