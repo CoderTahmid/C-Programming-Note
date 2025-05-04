d639a7fc7e44d651523c7cbfa21274f9591f4987
CoderTahmid <tahmid.ibne@gmail.com>
2025-05-04 08:53:03 +0600

    Usage of break keyword added

--- Loops in C Programming.c
+++ Loops in C Programming.c
@@ -7,8 +7,7 @@ int main()
 {
    int num = 0;
 
-   while (num <= 10)
-   {
+   while (num <= 10){
       printf("%d \n", num);
       num++;
    }
@@ -16,5 +15,42 @@ int main()
    This is a simple while loop structure
    */
 
+   return 0;
+}
+
+/*Using "break" keyword in While Loop*/
+
+int using_break_keyword_in_while_loop()
+{  
+   int n = 0;
+
+   while (n <= 100){
+      printf("%d\n", n);
+      n++;
+
+      if(n > 10){
+         break;
+      };
+   }
+   /*
+   Output: 
+   0
+   1
+   2
+   3
+   4
+   5
+   6
+   7
+   8
+   9
+   10
+
+   kono ekta loop e "break" keyword pawa gele 
+   sheta loop'r baire chole jay
+   ekhane jokhon e n er man 10'r boro hoise
+   tokhon e program ta loop'r baire chole geseh
+   */
+   
    return 0;
 }
--- practicePage.c
+++ practicePage.c
@@ -1,20 +1,38 @@
 #include <stdio.h>
-/*Loops in C Programming*/
 
-/*While Loop*/
+/*Using "break" keyword in While Loop*/
 
 int main()
-{
-   int num = 0;
+{  
+   int n = 0;
 
-   while (num <= 10)
-   {
-      printf("%d \n", num);
-      num++;
+   while (n <= 100){
+      printf("%d\n", n);
+      n++;
+
+      if(n > 10){
+         break;
+      };
    }
    /*
-   This is a simple while loop structure
-   */
+   Output: 
+   0
+   1
+   2
+   3
+   4
+   5
+   6
+   7
+   8
+   9
+   10
 
+   kono ekta loop e "break" keyword pawa gele 
+   sheta loop'r baire chole jay
+   ekhane jokhon e n er man 10'r boro hoise
+   tokhon e program ta loop'r baire chole geseh
+   */
+   
    return 0;
 }
