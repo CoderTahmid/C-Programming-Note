8aab564ec029d9ec0c1fe7456294e74bf911b9d8
CoderTahmid <tahmid.ibne@gmail.com>
2025-05-28 12:08:58 +0600

    concept about Bitwise NOT Operator added

--- Bit Wise Operators.c
+++ Bit Wise Operators.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    // bitwise and operator
+    // Bitwise AND Operator
     int x = 7, y = 14;
     int z = x & y;
     printf("%d\n", z); // output: 6
@@ -11,24 +11,27 @@ int main()
         (7)₁₀ = (0111)₂
         (14)₁₀ = (1110)₂
         eihanebasically binary digit gulo niye kaj hbeh
-        0111 er last'r digit 1110 er last digit er sathe and operation hbe
+        0111 er last'r digit 1110 er last digit er sathe AND operation hbe
         1st er sathe 1st ta
-        2nd tar sathe 2nd tar and operation hbe
+        2nd tar sathe 2nd tar AND operation hbe
         so if we do this we will get 
         for first digit --> 
-        0 and 1 = 0
-        1 and 1 = 1
-        1 and 1 = 1
-        1 and 0 = 0
+        0 AND 1 = 0
+        1 AND 1 = 1
+        1 AND 1 = 1
+        1 AND 0 = 0
         so the final result is 0110 which is 6 in decimal number system
         er jonne basically output 6 
         */
 
-    // ex-or operator
+    // Bitwise XOR Operator
     int c = x ^ y;
     printf("%d", c);
         /*
-        This ex-or operator same as and operator
+        This XOR operator same as and operator
         and e jebhabe kaj hoy ei ^ eo same bhabe kaj hbe
         */
+
+    // Bitwise NOT Operator
+    
 }
--- practicePage.c
+++ practicePage.c
@@ -1,29 +1,48 @@
 #include <stdio.h>
 
-// Using prototype in C
-
-float add( float a, float b);
-    /*
-    ebhabe prototype define na korle 
-    program kaj korbe na
-    error dekhabe
-    error message e bolbe
-    add is not defined
-    */
-
 int main()
 {
-    float a, b, c;
-    a = 2.7, b = 2.8;
-    c = add(a, b);
-    printf("%f", c);
-    
-    return 0;
-}
+    int x = 7, y = 14;
 
-float add( float num1, float num2 ) {
+    // Bitwise AND Operator
+    int a = x & y;
+    printf("a = %d\n", a); // output: 6
+        /*
+        so what's going on here, let's focus 
+        (7)₁₀ = (0111)₂
+        (14)₁₀ = (1110)₂
+        eihanebasically binary digit gulo niye kaj hbeh
+        0111 er last'r digit 1110 er last digit er sathe AND operation hbe
+        1st er sathe 1st ta
+        2nd tar sathe 2nd tar AND operation hbe
+        so if we do this we will get 
+        for first digit --> 
+        0 AND 1 = 0
+        1 AND 1 = 1
+        1 AND 1 = 1
+        1 AND 0 = 0
+        so the final result is 0110 which is 6 in decimal number system
+        er jonne basically output 6 
+        */
 
-    float sum = num1 + num2;
-    return sum;
+    // Bitwise XOR Operator
+    int b = x ^ y;
+    printf("b = %d\n", b); // output: b = 9
+        /*
+        This XOR operator same as and operator
+        and e jebhabe kaj hoy ei ^ eo same bhabe kaj hbe
+        */
 
+    // Bitwise NOT Operator
+    int c = ~x;
+    printf("c = %d", c); // output: c = -8
+        /*
+        So what it does?
+        the value of x is 7 right?
+        (7)₁₀ = (0111)₂
+        but ekhane c'r value hbe ei (0111) er one's compliment
+        orthat the new value will be (1000)
+        and 
+        (1000)₂ = (-8)₁₀
+        */
 }
