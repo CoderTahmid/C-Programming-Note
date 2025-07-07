105d61e5dad5cdeaf4ddd2249303b5830be95ca2
CoderTahmid <tahmid.ibne@gmail.com>
2025-07-07 09:32:32 +0600

    Problem 21 (Print a full pyramid) added

--- Problem & Solution list.c
+++ Problem & Solution list.c
@@ -414,3 +414,31 @@ int reverse_right_shifted_triangle() {
     }
     return 0;
 }
+
+// Problem 21--> Print a full pyramid
+/*
+   *
+  ***
+ *****
+*******
+*/
+int full_pyramid() {
+    
+    int i, n, j, k, l;
+    
+    printf("n = ");
+    scanf("%d", &n);
+    
+    for ( i = 1, j = 0 ; i <= n ; i++, j++) {
+        for ( k = 1 ; k <= n - i ; k++) {
+            printf(" ");
+        }
+        
+        for (l = 1 ; l <= i + j ; l++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
--- practicePage.c
+++ practicePage.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 
-// print a reverse right-shifted triangle 
 /*
-****
- ***
-  **
-   * 
+print a triangle like example given below 
+
+   *
+  ***
+ *****
+*******
 */
 
 int main() {
     
-    int i, n, j, k;
+    int i, n, j, k, l;
     
     printf("n = ");
     scanf("%d", &n);
     
-    for ( i = n ; i > 0 ; i--) {
-        for (k = 1 ; k <= n - i ; k++) {
+    for ( i = 1, j = 0 ; i <= n ; i++, j++) {
+        for ( k = 1 ; k <= n - i ; k++) {
             printf(" ");
         }
         
-        for ( j = 1 ; j <= i ; j++) {
+        for (l = 1 ; l <= i + j ; l++) {
             printf("*");
         }
-        
         printf("\n");
     }
+    
     return 0;
 }
