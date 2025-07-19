6729065a4b185c4e4ca420bb6d8c0bc2d06f5784
CoderTahmid <tahmid.ibne@gmail.com>
2025-07-19 15:43:56 +0600

    some changes done in pointer concept

--- Pointer in C.c
+++ Pointer in C.c
@@ -5,7 +5,18 @@
 int main()
 {
     int n = 10;
-    int *p = &n; // here p stores the address of n, &n diye basically ei variable'r address ta newa hocche
+    int *p = &n;
+    /*
+    here p stores the address of n, &n diye basically ei variable'r address ta newa hocche
+    kintu kotha hocche je
+    p jehetu ekta variable eta to normally rekhe dite partam as a variable
+    but keno er age ekta asterisk dilam?
+    karon hocche 
+    ei p variable e normal kono value stored hbe nah
+    ekhane ekta address store hbeh
+    and compiler ke to sheta bujhaite hbe ty nah?
+    er jonnei oikhane muloto asterisk dewa hoise 
+    */
     
     printf("%d\n", n); // prints the number
     printf("%p\n", &n); 
@@ -32,6 +43,11 @@ int main()
     0x7fcbe1126c --> ei address e gele 5 ke pawa jabe
     0x7fcbe1128s --> ei address e gele 'a' ke pawa jabe
     and these addresses are in hexadecimal number 
+    (
+        one thing to remember
+        whenever you are gonna print an address we will use %p
+        karon kono address'r format specifier hocche ei %p
+    )
     */
     printf("%p\n", p); // prints the same address as before 
     printf("%d\n", *p); 
@@ -48,6 +64,17 @@ int main()
     int *f = &k;
     *f = 20; // this changes the value of k from 5 to 20
     printf("the value of k: %d\n", k); // output: the value of k: 20
+
+    int j = 10;
+    int *king = &j;
+    printf("j = %p\n", &king); // output: j = 000000B2E3BFFC58
+    /*
+    so what's goin on here actually?
+    ekhane first e j er moddhe ekta value which is 10 store kora hoise
+    then king name e ekta variable e er address ta store kora hoise
+    now!!
+    we are exactly printing the address of king variable
+    */
 
     return 0;
 }
--- practicePage.c
+++ practicePage.c
@@ -1,45 +1,79 @@
 #include <stdio.h>
 
-// pass by value vs pass by reference
+// pointer in C programming 
 
-void normalChange(int n) {
-    n = 50;
-}
-
-void changeByPointer(int *p) {
-    *p = 500;
-}
-
-int main() {
-    int a = 5;
-    normalChange(a); // this is what we call call-by-value
-    printf("%d\n", a); // output: 5
+int main()
+{
+    int n = 10;
+    int *p = &n;
     /*
-    as we can see the value has not changed
-    value 5 e roye geche
-    er karon hocche  je
-    amra jokhon a variable ta ke normalChange function e pathacchi
-    ekhane muloto jacche only a copy of the variable
-    not the real variable
-    mane a er ekta copy jacche oi function e, and oi function e giye o or moto kaj kortase
-    real a value ta jacche na
-    er jonnei a er value ta change hoy naih
-    and eta kei bole call by value
+    here p stores the address of n, &n diye basically ei variable'r address ta newa hocche
+    kintu kotha hocche je
+    p jehetu ekta variable eta to normally rekhe dite partam as a variable
+    but keno er age ekta asterisk dilam?
+    karon hocche 
+    ei p variable e normal kono value stored hbe nah
+    ekhane ekta address store hbeh
+    and compiler ke to sheta bujhaite hbe ty nah?
+    er jonnei oikhane muloto asterisk dewa hoise 
     */
-
-    int b = 10;
-    int *p = &b;
-    changeByPointer(p);
-    printf("%d\n", b); // output: 500
+    
+    printf("%d\n", n); // prints the number
+    printf("%p\n", &n); 
     /*
-    so ekhane ja hocche ta holo je
-    b er memory address (which is stored in p) passed to the function changeByPointer()
-    eibar shei changeByPointer function e
-    shei address e new ekta value write kora hocche(which is 500)
-    ebar kintu ager moto variable'r copy pathano hocche nah
-    variable'r address pass kora hocche 
-    and shei address dhore she value change kortase
-    and this is what we call-by-reference
+    output: 0x7fcbe1126c
+    So is happening here?
+    ekhane muloto oi n variable ta memory'r kon
+    address e ache sheita print kortase
+    0x7fcbe1126c is a memory address actually 
+    
+    what is memory address actually!?
+    amra jokhon code e kono data store kori 
+    eta RAM'r ekta specific memeory cell e save hoy
+    and shei specific memory cell'r address 
+    tai hocche eita
+    like
+    if we imagine the computer's RAM as a big hotel
+    each room in this hotel is a memory address 
+    each room can store dayawhatever it want
+    like
+    int a = 5
+    char ch = 'a';
+    ekhn dhori
+    0x7fcbe1126c --> ei address e gele 5 ke pawa jabe
+    0x7fcbe1128s --> ei address e gele 'a' ke pawa jabe
+    and these addresses are in hexadecimal number 
+    (
+        one thing to remember
+        whenever you are gonna print an address we will use %p
+        karon kono address'r format specifier hocche ei %p
+    )
+    */
+    printf("%p\n", p); // prints the same address as before 
+    printf("%d\n", *p); 
+    /*
+    output: 10
+    eta ke dereferencing 
+    ekhane oi address e thaka value ta ke print kora hocche
+    etai basically dereferencing 
+    */
+    
+    // pointer assignment and dereferencing 
+    
+    int k = 5;
+    int *f = &k;
+    *f = 20; // this changes the value of k from 5 to 20
+    printf("the value of k: %d\n", k); // output: the value of k: 20
+
+    int j = 10;
+    int *king = &j;
+    printf("j = %p\n", &king); // output: j = 000000B2E3BFFC58
+    /*
+    so what's goin on here actually?
+    ekhane first e j er moddhe ekta value which is 10 store kora hoise
+    then king name e ekta variable e er address ta store kora hoise
+    now!!
+    we are exactly printing the address of king variable
     */
 
     return 0;
