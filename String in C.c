bd9fa9699625dc35ccd4ffc72a4062a764a3b595
CoderTahmid <tahmid.ibne@gmail.com>
2025-07-24 22:17:32 +0600

    concept of strcmp added

--- String in C.c
+++ String in C.c
@@ -42,5 +42,33 @@ int main()
     NB: firstStr er ototuk storage capacity thakte hbe jate she 2 tai store korte pare
     */
 
+    // Usage of strcmp()
+    char prothomStr[] = "Apple";
+    char ditiyoStr[] = "Banana";
+    int cmpVal = strcmp(prothomStr, ditiyoStr);
+    printf("%d\n", cmpVal); // output: -60
+    /*
+    ekhane first string'r sathe second string'r compare korbe
+    and ei comparison ta chole character gular ASCII code'r upre base kore
+    first string ta jodi boro ( talking about ASCII value ) hoy compared to 2nd string
+    tahole +ve return korbe
+    first string choto hoile -ve return korbe
+    and both are equal hoile 0
+
+    now ei comparison ta actually hoy kibhabe?
+    there's a different process for this
+    first string'r first okkhor'r sathe
+    2nd string'r first okkhor milay
+    dekhe kontar ASCII boro
+    jetar ASCII boro shetai boro hishabe bibechito hbe
+
+    ekhane A = 65
+    B = 66
+    A choto houa te negative return korse
+
+    jodi first okkhor same hoy tahole next okkhor compare korbe
+    ebhabe pura process ta cholbe
+    */
+
     return 0;
 }
