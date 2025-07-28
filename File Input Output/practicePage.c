0762d50caa40f20dc4b139e99a8c0dc0e38ac0e5
CoderTahmid <tahmid.ibne@gmail.com>
2025-07-28 08:58:25 +0600

    the defination of 'FILE' about file input output added

---
+++ File Input Output/practicePage.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+    FILE *fptr; 
+        /*
+        So what we are doing here in this line
+        ekhane FILE hocche ekta structure jeta kina stdio.h e define kora ache
+        amra ei FILE structure tar address fptr e store kortasi
+
+        what is this FILE exactly?
+        basically FILE is something that we need for work with a file, like file closing or opening 
+        we programmers do not need to know about the defination of this FILE
+        it is hidden from us
+        amra ei fptr e FILE structure'r address store kore kisu task perform kori arki
+        */
+    return 0;
+}
