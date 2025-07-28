680fd43ccacfee91ad6ab94a8025a030c04aea18
CoderTahmid <tahmid.ibne@gmail.com>
2025-07-28 09:34:04 +0600

    concepts about FILE mode added

--- File Input Output/practicePage.c
+++ File Input Output/practicePage.c
@@ -1,5 +1,53 @@
 #include <stdio.h>
 
+/*
+File opening modes
+fopen(file name, mode);
+amra ekhane kisu mode pass kori, 
+they are
+
+"r" -->
+open to read
+now suppose we are doing this 
+fptr = fopen("king.txt", "r");
+ekhane kintu king.txt name e kono file nai
+tai fptr e ekta null value set hbe
+
+"rb" -->
+open to read in binary
+same thing will happen what will happen in "r" 
+if we try to open a file that doesn'y exist
+
+"w" -->
+open to write
+dhorlam 
+amra emon ekta file e kichu write korte chai
+jeta kina exist kore nah
+tahole ei "w" jeta korbe ta holo je
+she oi name'r ekta file nije create kore nibe
+then write korbe
+
+ar jodi ekta existing file e amra write kori
+tahole jeta hbe
+shei file ta open hbe and oi file e thaka previous data gulo delete hoye jabe
+and new written data gulo ashbe sekhane 
+
+"wb" -->
+open to write in binary
+the thing I meantioned about about 
+if file exist 
+if not
+in the "w" section
+same thing will happen here also
+
+"a" -->
+open to append
+append mane jog kora
+like amra emon intention e file ta open korlam
+jekhane previous data'r sathe new data gula append hbe
+ete ager data o thakbe and new data gulao ashbe
+*/
+
 int main () {
     FILE *fptr; 
         /*
@@ -13,5 +61,14 @@ int main () {
         it is hidden from us
         amra ei fptr e FILE structure'r address store kore kisu task perform kori arki
         */
+
+    fptr = fopen("test.txt", "r");
+        /*
+        here in this line
+        we are telling the compiler to open the file named test.txt
+        and the intention of opening the file is reading it, as we pass the 2nd value "r"
+        "r" represents reading
+        */
+    
     return 0;
 }
