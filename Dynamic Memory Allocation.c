4732aa14b762399010854c909fee1903e9de1fc4
CoderTahmid <tahmid.ibne@gmail.com>
2025-08-02 19:39:55 +0600

    concept about calloc added

--- Dynamic Memory Allocation.c
+++ Dynamic Memory Allocation.c
@@ -30,5 +30,30 @@ int main()
     ptr[4] = 2323; // this how we can set the value of this ptr, just like normal array
     printf("%d\n", ptr[4]); // output: 2323
 
+    // calloc() function
+    /*
+    calloc means continous alloction
+    calloc() e ekta bishoy hocche je
+    amra jokhon calloc diye kichu memory nibo tokhon ogula by defaul first e 0 thakbe each values
+    like
+    9 byte nilam 
+    3 ta integer
+    in initial moment
+    n[0] = 0
+    n[1] = 0
+    n[2] = 0
+    
+    calloc() syntax
+    calloc(how many locations, sizeof(data type))
+    first parameter e dite hbe je koyta location lgbe
+    and 2nd parameter e data type's size dite hbe
+    */
+    int *ptr1;
+    ptr1 = (int*) calloc(4, sizeof(int)); // calloc o void return kore, ty etake int e convert kore nite hbe
+
+    ptr1[1] = 10;
+    ptr1[2] = 20;
+    printf("ptr[0] = %d\tptr[1] = %d\n", ptr1[0], ptr1[1]); // output: ptr[0] = 0      ptr[1] = 10
+
     return 0;
 }
